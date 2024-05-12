102ceb0b88f1cb3eb74cf94e3db5037521ad2ef2
Obibaz <117935653+Obibaz@users.noreply.github.com>
2024-05-12 10:04:05 +0300

    Додано модель + тест роботи злиття

---
+++ Models/Decision.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Decision
+    {
+        public int Id { get; set; }
+        public int Index { get; set; }
+        public DateTime Date { get; set; }
+        public string Form { get; set; }
+        public string Type { get; set; }
+
+        public int CourtId { get; set; }
+        public virtual Court Court { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Pass { get; set; }
+        public bool Active { get; set; }
+        public string Status { get; set; }
+
+        public virtual ICollection<Court> Courts { get; set; }
+    }
+}
